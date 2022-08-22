06ff9c2b2beb03163b5c3d64c4fbccf361b8a9ff
pete_ <Petri.Kallinen@metropolia.fi>
2022-08-22 15:03:35 +0300

    Kokonaisluku laskuri

---
+++ Teht_2.4.py
@@ -0,0 +1,15 @@
+#luvut
+
+Luku_1 = int(input("Anna 1. kokonaisluku"))
+Luku_2 = int(input("Anna 2. kokonaisluku"))
+Luku_3 = int(input("Anna 3. kokonaisluku"))
+
+#laskutoimitukset
+summa = Luku_1 + Luku_2 + Luku_3
+tulo = Luku_1 * Luku_2 * Luku_3
+ka = summa / 3
+
+#tulostus
+print(summa)
+print(tulo)
+print(ka)
