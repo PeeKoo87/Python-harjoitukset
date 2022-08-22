f8a33c3fa820514caa316ce835c8f34f3cf22bb9
pete_ <Petri.Kallinen@metropolia.fi>
2022-08-22 14:45:27 +0300

    Suorakaide

---
+++ Teht_2.3.py
@@ -0,0 +1,13 @@
+# kysytään tiedot
+
+Kanta = float(input("Anna suorakulmion kanta"))
+Korkeus = float(input("Anna suorakulmion korkeus"))
+
+#laskutoimet
+A = Kanta * Korkeus
+
+Piiri = (Kanta * 2) + (Korkeus * 2)
+
+#tulostukset
+print(f"ala = {A}")
+print(f"Piiri = {Piiri}")
