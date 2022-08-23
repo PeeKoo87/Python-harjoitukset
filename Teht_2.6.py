adc81dc9bad29f6b8b14b887c50eb198d2c9caa3
pete_ <Petri.Kallinen@metropolia.fi>
2022-08-23 17:27:44 +0300

    eka commit

---
+++ Teht_2.6.py
@@ -0,0 +1,15 @@
+import random
+a = random.randint(0, 9)
+b = random.randint(0, 9)
+c = random.randint(0, 9)
+
+print("satunnaiset 3 lukua")
+print(a, b, c)
+
+x = random.randint(1, 6)
+y = random.randint(1, 6)
+z = random.randint(1, 6)
+q = random.randint(1, 6)
+
+print("satunnaiset 4 lukua")
+print(x, y, z, q)
